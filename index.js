7901c2424a63c6d0cfe666ab8566aa261442d46a
hyurl <i@hyurl.com>
2018-06-06 18:54:24 +0800

    change the way to serve static files

--- index.js
+++ index.js
@@ -1,43 +1,13 @@
 const { App } = require("webium");
-const fs = require("fs-extra");
-const { endsWith } = require("lodash");
+const serveStatic = require("serve-static");
 
 var app = new App();
 
-app.get("/(.*)", async (req, res) => {
-    let filename = process.cwd() + req.pathname,
-        exists = await fs.pathExists(filename);
+app.use(serveStatic(__dirname, {
+    extensions: ["html", "htm"],
+    index: ["index.html", "index.htm"]
+}));
 
-    if (exists) {
-        let stats = await fs.stat(filename);
-
-        if (stats.isDirectory()) {
-            if (!endsWith(filename, "/")) {
-                req.urlObj.pathname += "/";
-                return res.redirect(req.urlObj.href);
-            } else {
-                filename += "index.html";
-
-                exists = await fs.pathExists(filename);
-
-                if (exists)
-                    return res.sendFile(filename);
-            }
-        } else if (stats.isFile) {
-            return res.sendFile(filename);
-        }
-    } else if (!endsWith(filename, "/")) {
-        filename += ".html";
-        exists = await fs.pathExists(filename);
-
-        if (exists)
-            return res.sendFile(filename);
-    }
-
-    res.status = 404;
-    res.send(res.status);
-});
-
-app.listen(80, () => {
-    console.log("HTTP Server started at http://localhost");
+app.listen(3000, () => {
+    console.log("HTTP Server started at http://localhost:3000");
 });
