2013c4de475a4652de68709671ee0e569a1499ad
hyurl <i@hyurl.com>
2018-08-14 15:15:08 +0800

    update css and js

--- js/common.js
+++ js/common.js
@@ -5,11 +5,11 @@
 "use strict";
 
 String.byteLength = function byteLength(str) {
-    let b = 0,
+    var b = 0,
         l = str.length;
 
     if (l) {
-        for (let i = 0; i < l; i++) {
+        for (var i = 0; i < l; i++) {
             if (str.charCodeAt(i) > 255) {
                 b += 2;
             } else {
@@ -22,17 +22,16 @@ String.byteLength = function byteLength(str) {
     return 0;
 }
 
-var path = location.pathname,
-    hash = location.hash,
-    match = location.search.match(/lang=(.*)[&]*/),
-    lang = match ? match[1] : navigator.language,
-    isZH = lang == "zh-CN",
-    pin = `<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`;
+var Pathname = location.pathname,
+    Hash = location.hash,
+    QueryLangMatches = location.search.match(/[?|&]lang=(.*)[&]*/),
+    Lang = QueryLangMatches ? QueryLangMatches[1] : navigator.language,
+    Pin = `<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`;
 
 location.hash = "";
 
 function onRendered() {
-    let navbar = $(".navbar>ul"),
+    var navbar = $(".navbar>ul"),
         sidebar = $(".sidebar"),
         content = $("article.content"),
         renderer = new marked.Renderer(),
@@ -42,13 +41,13 @@ function onRendered() {
 
     //Render markdown headings.
     renderer.heading = function (text, level) {
-        let isLatin = String.byteLength(text) == text.length,
+        var isLatin = String.byteLength(text) == text.length,
             _text = text.replace(/\s/g, '-'),
             re = /[~`!@#\$%\^&\*\(\)\+=\{\}\[\]\|:"'<>,\.\?\/]/g,
             id;
 
         if (isLatin) {
-            let matches = _text.match(/[\-0-9a-zA-Z]+/g);
+            var matches = _text.match(/[\-0-9a-zA-Z]+/g);
             id = matches ? matches.join("_") : _text.replace(re, "_");
         } else {
             id = _text.replace(re, "_");
@@ -56,12 +55,12 @@ function onRendered() {
 
         id = StringTrimmer.trim(id, "_");
 
-        return `<h${level} id="${id}"><a class="heading-anchor" href="#${id}">${pin}</a>${text}</h${level}>\n`;
+        return `<h${level} id="${id}"><a class="heading-anchor" href="#${id}">${Pin}</a>${text}</h${level}>\n`;
     };
 
     //Render markdown codes to be highlighted.
     renderer.code = function (code, lang, escaped) {
-        return `<pre><code class="lang-${lang} hljs">${hljs.highlightAuto(code).value}</code></pre>`;
+        return `<pre><code class="lang-${lang} hljs">${hljs.highlight(lang, code, true).value}</code></pre>`;
     };
 
     // toggle navbar
@@ -73,8 +72,8 @@ function onRendered() {
     navbar.children().each(function () {
         var $this = $(this),
             link = $this.children("a").attr("href"),
-            i = path.lastIndexOf("/") + 1,
-            cat = i ? path.substr(0, i) : path;
+            i = Pathname.lastIndexOf("/") + 1,
+            cat = i ? Pathname.substr(0, i) : Pathname;
 
         link == cat ? $this.addClass("active") : $this.removeClass("active");
     });
@@ -82,7 +81,7 @@ function onRendered() {
     //Highlight sidebar tab
     sidebar.find("a").each(function () {
         var href = $(this).attr("href");
-        if (href == path) {
+        if (href == Pathname) {
             $(this).addClass("active");
         }
     });
@@ -105,12 +104,12 @@ function onRendered() {
     });
 
     //Scroll the document.
-    if (hash) {
-        $('a[href="' + hash + '"]').click();
+    if (Hash) {
+        $('a[href="' + Hash + '"]').click();
     }
 
     /** Type installing command. */
-    let command = $(".command>pre"),
+    var command = $(".command>pre"),
         type = function () {
             command.typeIn("slow", function () {
                 setTimeout(type, 1500);
@@ -120,9 +119,8 @@ function onRendered() {
         setTimeout(type, 1500);
 
     // Add ?lang=[lang] in the URL
-    let replaceLink = function (target) {
-        let match = location.search.match(/lang=(.*)[&]*/);
-        if (match) {
+    var replaceLink = function (target) {
+        if (QueryLangMatches) {
             target = target || $("body");
             target.find("a").each(function () {
                 var href = $(this).attr("href");
@@ -131,9 +129,9 @@ function onRendered() {
                     href.indexOf("#") !== 0 &&
                     href != "?lang=zh-CN" &&
                     href != "?lang=en-US") {
-                    $(this).attr("href", href.split("?")[0] + "?lang=" + match[1]);
+                    $(this).attr("href", href.split("?")[0] + "?lang=" + Lang);
                 }
-            })
+            });
         }
     };
     replaceLink();
@@ -142,35 +140,35 @@ function onRendered() {
     if (content.length) {
         SoftLoader.bind(content[0]);
 
-        let getMarkdownPath = (url) => {
-            let start = url.lastIndexOf("/") + 1,
+        var getMarkdownPath = (url) => {
+            var start = url.lastIndexOf("/"),
                 end = url.lastIndexOf("."),
                 dirname = url.substring(0, start),
-                basename = url.substring(start, end != -1 ? end : url.length);
+                basename = url.substring(start + 1, end != -1 ? end : url.length);
 
-            return dirname + (lang == "zh-CN" ? "zh-CN/" : "en-US/") + basename;
+            return dirname + "/" + Lang + "/" + basename;
         };
-        let getContent = (url, title = document.title) => {
+        var getContent = (url, title = document.title) => {
             $.get(url + ".md", (data) => {
                 content.removeClass("fadeOut").addClass("fadeIn");
 
-                url = url.replace(/docs\/[a-zA-Z-]+\//, "docs/");
-                if (/lang=(.*)[&]*/.test(location.search))
-                    url += "?lang=" + lang;
+                url = url.replace(/docs\/.+?\//, "docs/");
+                if (QueryLangMatches)
+                    url += "?lang=" + Lang;
 
                 SoftLoader.replaceWith(parseMarkdown(data), title, url);
                 replaceLink(content);
             });
         };
 
-        getContent(getMarkdownPath(path), document.title);
+        getContent(getMarkdownPath(Pathname), document.title);
 
         sidebar.find("a").filter(function () {
             return /^javascript\:/.test($(this).attr("href")) == false;
         }).click(function (event) {
             event.preventDefault();
 
-            let $this = $(this),
+            var $this = $(this),
                 href = $this.attr("href").split("?")[0],
                 text = $this[0].innerText || $this[0].textContent,
                 title = $this.attr("data-title");//document.title.replace(/:.+\|/, `: ${text} |`);
@@ -187,14 +185,14 @@ function onRendered() {
 whatstpl.Template.register("/layout.html", `
 <header class="header vivify popInTop" id="header">
     <div class="logo">
-        <a href="/@{module}">@{ moduleName }</a>
+        <a href="/">@{ moduleName }</a>
     </div>
     <div class="change-lang">
         <a href="@{ lang }">@{ langLabel }</a>
     </div>
     <div class="navbar">
         <ul>
-            <for statement="let url in navbarMenu">
+            <for statement="var url in navbarMenu">
                 <li><a href="@{ url }">@{ navbarMenu[url] }</a></li>
             </for>
         </ul>
@@ -265,8 +263,8 @@ whatstpl.Template.register(`/components.html`, `
     <div class="container vivify fadeIn">
         <aside class="sidebar vivify fadeIn">
             <ul>
-                <for statement="let filename in sidebarMenu">
-                    <li><a href="/@{module}/docs/@{filename}" data-title="@{ sidebarMenu[filename].title } | @{moduleName}">@{ sidebarMenu[filename].label }</a></li>
+                <for statement="var filename in sidebarMenu">
+                    <li><a href="/docs/@{filename}" data-title="@{ sidebarMenu[filename].title } | @{moduleName}">@{ sidebarMenu[filename].label }</a></li>
                 </for>
             </ul>
         </aside>
@@ -276,7 +274,7 @@ whatstpl.Template.register(`/components.html`, `
 `);
 
 $(function () {
-    let app = $("#app"),
+    var app = $("#app"),
         tpl = app.html();
 
     new whatstpl.Template("app.html").render(tpl, window.locals).then(html => {
